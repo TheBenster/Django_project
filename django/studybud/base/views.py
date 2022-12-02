c309fda18fc658d992acaadafe184ef30b65ac2e
Ben Beaver <bbeaver93@gmail.com>
2022-12-02 12:15:51 -0500

    Added room form, edit, and delete

--- django/studybud/base/views.py
+++ django/studybud/base/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
-'''
-rooms = [
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'Front end devs'},
-    {'id':3, 'name':'Design'}
-] '''
 
 def home(request):
     rooms = Room.objects.all()
@@ -20,6 +15,33 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
-    context = {}
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance = room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
