faf85bbcaa0bb79aef35ee4c33c4ac7267197362
Ben Beaver <bbeaver93@gmail.com>
2022-12-05 11:07:14 -0500

    Created login page

--- django/studybud/base/views.py
+++ django/studybud/base/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
+from django.contrib.auth import authenticate, login, logout
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
+    topics = Topic.objects.all()
+    rooms_count = rooms.count
+
+    context = {'rooms': rooms, 'topics':topics, 'rooms_count' : rooms_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -45,3 +60,24 @@ def delete_room(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try: 
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist.')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+        else:
+            messages.error(request, 'Username or password does not exist')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logout(request):
+    logout(request)
+    return redirect('home')
